666f15164c5fa2c7f71d5d42070d69bb3f26425b
julen <jleizza@kopiustech.com>
2026-02-09 15:39:59 -0300

    modified users entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn} from 'typeorm'
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, OneToMany} from 'typeorm'
 
 @Entity({name:'users'})
 export class User {
@@ -8,11 +8,28 @@ export class User {
     @Column({unique: true})
     email: string
 
+    @Column({unique: true})
+    userName: string
+
+    @Column({unique: true})
+    password: string
+
+    @Column({nullable: true})
+    profileImage?: string
+
+
     @CreateDateColumn()
     createdAt: Date
 
     @UpdateDateColumn()
     lastUpdatedAt: Date
+
+
+    // @OneToMany(() => Game, (game) => game.owner)
+    // games: Game[]
+    
+    // @OneToMany(() => Favourites, (favourite) => favourite.user)
+    // favourites: Favourite[]
 
     @DeleteDateColumn({select: false})
     deletedAt: Date | null
