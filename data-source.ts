9b07d234729197611536fdef6579f994099cce3d
julen <jleizza@kopiustech.com>
2026-02-09 14:03:07 -0300

    made the user entity, re-scaffolded the users module

---
+++ data-source.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import { User } from './src/user/entities/user.entity'; 
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: 'password',
+    database: 'mi_db',
+    entities: [User], 
+    migrations: ['./src/migrations/*.ts'],
+});
