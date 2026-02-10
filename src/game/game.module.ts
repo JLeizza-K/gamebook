64fdfc86a2c531719027bcbbb578d868a8a63905
julen <jleizza@kopiustech.com>
2026-02-10 16:31:17 -0300

    added final fixes, now app runs normally

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Game } from 'src/entities/game.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Game])],
   controllers: [GameController],
   providers: [GameService],
 })
